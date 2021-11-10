2a560418e689b103860e3ff02adb830ba074e8a0
souptik4572 <souptiksarkar4572@gmail.com>
2021-11-10 12:14:56 +0530

    added local postgres connection

--- justaskit/settings.py
+++ justaskit/settings.py
@@ -87,6 +87,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'justaskit',
+#         'USER': 'souptiksarkar',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
