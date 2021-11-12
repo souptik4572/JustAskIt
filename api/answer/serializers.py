e0a8c80078ef844800a8abc0f9cf54d3786f5553
souptik4572 <souptiksarkar4572@gmail.com>
2021-11-12 08:06:21 +0530

    added question serializer for populating answer serializer

--- api/answer/serializers.py
+++ api/answer/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import Answer
+from ..question.serializers import QuestionSerializer
 
 
 class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    question = QuestionSerializer(read_only=True)
+
     class Meta:
         model = Answer
-        fields = ('id', 'answer', 'created_at', 'updated_at')
+        fields = ('id', 'question', 'answer', 'created_at', 'updated_at')
