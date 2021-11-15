e47a2e2a1fb06eb545824aeaf3fbf7c95cb6dcc5
souptik4572 <souptiksarkar4572@gmail.com>
2021-11-15 10:05:40 +0530

    added further authorization to answer get requests

--- api/answer/urls.py
+++ api/answer/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import create_new_answer, edit_existing_answer, delete_existing_answer, get_all_answers, get_answer_to_particular_question
+from .views import create_new_answer, edit_existing_answer, delete_existing_answer, get_all_answers, get_answers_to_particular_question
 
 urlpatterns = [
     path('all/', get_all_answers, name='api.get_all_answers'),
-    path('question/<int:question_id>/', get_answer_to_particular_question,
-         name='api.get_answer_to_particular_question'),
+    path('to-question/<int:question_id>/', get_answers_to_particular_question,
+         name='api.get_answers_to_particular_question'),
     path('new/to-question/<int:question_id>/',
          create_new_answer, name='api.create_new_answer'),
     path('<int:answer_id>/edit/', edit_existing_answer,
--- api/answer/views.py
+++ api/answer/views.py
@@ -1,10 +1,13 @@
 from django.http import JsonResponse
-from django.utils.decorators import decorator_from_middleware
+from django.db.models import Q
+from django.utils.decorators import decorator_from_middleware, decorator_from_middleware_with_args
 from rest_framework.decorators import api_view
 from rest_framework import status
+from api.constants.ask_type_constants import LIMITED, PUBLIC
 from .serializers import AnswerSerializer
 from .models import Answer
 from ..question.models import Question
+from ..user.models import Follow
 from django.views.decorators.csrf import csrf_exempt
 import json
 from ..middleware.auth_strategy import AuthStrategyMiddleware
@@ -14,13 +17,26 @@ from ..middleware.auth_strategy import AuthStrategyMiddleware
 
 @csrf_exempt
 @api_view(['GET'])
+@decorator_from_middleware_with_args(AuthStrategyMiddleware)(False)
 def get_all_answers(request):
     try:
-        answers = Answer.objects.all()
+        if request.user is None:
+            # If no user is logged, we fetch all answers whose questions are public
+            answers = Answer.objects.filter(question__ask_type=PUBLIC).all()
+        else:
+            # If user is logged, we fetch only those answers whose questions are limited and which are posted by some followee user to our logged user
+            answers_to_public_questions = Answer.objects.filter(
+                question__ask_type=PUBLIC).all()
+            followees = Follow.objects.filter(
+                follower__id=request.user.id).values('id')
+            answers_to_limited_questions = Answer.objects.filter(
+                Q(question__owner__id__in=followees) | Q(question__owner__id=request.user.id))
+            #  combining both of the answers
+            answers = answers_to_public_questions | answers_to_limited_questions
         return JsonResponse({
             'success': True,
             'message': 'All answers of the portal',
-            'answers': AnswerSerializer(answers, many=True)
+            'answers': AnswerSerializer(answers, many=True).data
         })
     except Exception as e:
         return JsonResponse({
@@ -31,14 +47,39 @@ def get_all_answers(request):
 
 @csrf_exempt
 @api_view(['GET'])
-def get_answer_to_particular_question(request, question_id):
+@decorator_from_middleware_with_args(AuthStrategyMiddleware)(False)
+def get_answers_to_particular_question(request, question_id):
     try:
-        answers = Answer.objects.filter(question__id=question_id).all()
+        the_question = Question.objects.get(id=question_id)
+        if request.user is None:
+            if the_question.ask_type == LIMITED:
+                return JsonResponse({
+                    'success': False,
+                    'message': 'Access blocked to unauthorized user'
+                }, status=status.HTTP_400_BAD_REQUEST)
+            answers = Answer.objects.filter(question__id=question_id).all()
+        else:
+            if not Follow.objects.filter(followee=the_question.owner.id, follower=request.user.id).exists() and the_question.ask_type == LIMITED and the_question.owner.id != request.user.id:
+                return JsonResponse({
+                    'success': False,
+                    'message': 'Question is limited and can only be accessed by followers of the owner'
+                }, status=status.HTTP_400_BAD_REQUEST)
+            answers = Answer.objects.filter(question__id=question_id).all()
         return JsonResponse({
             'success': True,
             'message': f"All answers of particular question with id {question_id}",
             'questions': AnswerSerializer(answers, many=True).data
         }, status=status.HTTP_200_OK)
+    except Question.DoesNotExist:
+        return JsonResponse({
+            'success': False,
+            'message': 'Question does not exist'
+        }, status=status.HTTP_404_NOT_FOUND)
+    except Follow.DoesNotExist:
+        return JsonResponse({
+            'success': False,
+            'message': 'Logged user not eligible to view answers to this question'
+        }, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
         return JsonResponse({
             'success': False,
